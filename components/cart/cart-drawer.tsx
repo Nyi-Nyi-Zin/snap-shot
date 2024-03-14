261030955c3e5c16983dbcccf6612b3e9f437206
Miracle <nyinyizin1818@gmail.com>
2024-03-14 10:00:00 +0630

    cart-status

--- components/cart/cart-drawer.tsx
+++ components/cart/cart-drawer.tsx
@@ -9,11 +9,14 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import CartItem from "./cart-item";
+import CartStatus from "./cart-status";
+import { useCartStore } from "@/store/cart-store";
 
 type CartDrawerProps = {
   children: React.ReactNode;
 };
 const CartDrawer = ({ children }: CartDrawerProps) => {
+  const cartPosition = useCartStore((state) => state.cartPosition);
   return (
     <>
       <Drawer>
@@ -22,8 +25,10 @@ const CartDrawer = ({ children }: CartDrawerProps) => {
           <DrawerHeader>
             <DrawerTitle>Your Cart</DrawerTitle>
             <DrawerDescription>Stay home.Stay safe.</DrawerDescription>
+            <CartStatus />
           </DrawerHeader>
-          <CartItem />
+          {cartPosition === "Order" && <CartItem />}
+          {cartPosition === "Checkout" && <p>I am order element.</p>}
         </DrawerContent>
       </Drawer>
     </>
